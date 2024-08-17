0e30b5628aba536266dbb0e8164ccd7e66c3051e
Javier Corado <javiercoradofp@gmail.com>
2024-08-16 20:12:59 -0600

    feat(model): add article pydantic model

---
+++ river_compass/models/article.py
@@ -0,0 +1,21 @@
+"""Python module for the article pydantic model"""
+
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class Article(BaseModel):
+    """
+    A model representing an article.
+
+    Attributes:
+        name (str): The name of the article.
+        price (float): The price of the article.
+        description (Optional[str]): An optional description of the article.
+    """
+
+    name: str = Field(..., description="The name of the article.")
+    price: float = Field(..., description="The price of the article.")
+    description: Optional[str] = Field(
+        None, description="An optional description of the article."
+    )
